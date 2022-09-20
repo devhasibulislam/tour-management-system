c5a09d31105f37da96d4f7576696be6773dbfa28
hasibulislam999 <hasib143sl@gmail.com>
2022-09-21 00:14:29 +0600

    driver code modified through essence

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 /**
  * Title: Tour Management System
- * Description: Create a system that manages tours for a user
+ * Description: Create a system that contains several endpoints.
  * Author: Hasibul Islam
  * Date: 20/09/2022
  */
+
+/* external imports */
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+/* internal imports */
+const errorHandler = require("./middlewares/error.middleware");
+const dbConnection = require("./utils/db.util");
+
+/* application connection */
+const app = express();
+const port = process.env.PORT;
+
+/* middleware connection */
+app.use(cors());
+app.use(express.json());
+
+/* router connection */
+
+/* global error handler */
+app.use(errorHandler);
+
+/* DB connection */
+dbConnection();
+
+/* enable backend */
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    acknowledgement: true,
+    title: "Tour Management System",
+    message: "OK",
+    description: "TMS request on server working.",
+  });
+});
+
+/* enable port */
+app.listen(port, () => {
+  console.log(`TMS listening on port ${port}.`);
+});
